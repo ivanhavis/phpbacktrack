95527f1b22ee3559895e4c8a27fc4d7e56c672e7
ivanhavis <ihzahavis@gmail.com>
2025-07-25 22:19:30 +0800

    2ndtry

--- barang/index.php
+++ barang/index.php
@@ -18,38 +18,55 @@ echo '</script>';
 </head>
 
 <body>
- <div class="1-form">
-<div class="shape1"></div>
- <div class="shape2"></div>
- <div class="form">
-<lottie-player class="form_img" src="https://assets5.lottiefiles.com/packages/1f20_mrmg8x7w.json"
- background="transparent" speed="1" style="width: 580px; height: 500px;"
- loop autoplay></lottie-player>
+  <div class="l-form">
+    <div class="shape1"></div>
+    <div class="shape2"></div>
 
-<form id="login" method="post" name="login" action="login_proses.php" class="form content">
- <h1 class="form_title">Welcome</h1>
- <div class="form_div form_div-one">
- <div class="form_icon">
- <i class='bx bx-user-circle'></i>
- </div>
- <div class="form_div-input">
- <label for="" class="form_label">Username</label>
- <input name="username" type="text" class="form_input" id="username">
- </div>
- <div class="form_div">
- <div class="form_icon">
- <i class='bx bx-lock' ></i>
- </div>
- <div class="form_div-input">
- <label for="" class="form_label">Password</label>
- <input name="password" type="password" class="form_input" id="password">
- </div>
-</div>
-<br><br>
-<input name="login" type="submit" class="form_button" id="login" value="Login">
-</form>
-</div>
-</div>
-<script src="assets/js/main.js"></script>
+    <div class="form">
+      <div class="form__img">
+    <img src="../gambar/Timeline 1.gif" alt="Welcome" class="welcome-gif" width="400" height="200"  />
+      </div>
+
+      <form id="login" method="post" name="login" action="login_proses.php" class="form__content">
+        <h1 class="form__title"></h1>
+
+        <div class="form__div form__div-one">
+          <div class="form__icon">
+            <i class='bx bx-user-circle'></i>
+          </div>
+          <div class="form__div-input">
+            <input name="username" type="text" class="form__input" id="username" placeholder=" " required>
+            <label for="username" class="form__label">Username</label>
+          </div>
+        </div>
+
+        <div class="form__div">
+          <div class="form__icon">
+            <i class='bx bx-lock'></i>
+          </div>
+          <div class="form__div-input">
+            <input name="password" type="password" class="form__input" id="password" placeholder=" " required>
+            <label for="password" class="form__label">Password</label>
+          </div>
+        </div>
+
+        <input name="login" type="submit" class="form__button" id="login" value="Login">
+      </form>
+    </div>
+  </div>
+
+  <script>
+    const inputs = document.querySelectorAll('.form__input');
+    inputs.forEach(input => {
+      input.addEventListener('focus', () => {
+        input.parentNode.parentNode.classList.add('focus');
+      });
+      input.addEventListener('blur', () => {
+        if (input.value === "") {
+          input.parentNode.parentNode.classList.remove('focus');
+        }
+      });
+    });
+  </script>
 </body>
 </html>
